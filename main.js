f1580fbe3fb39f410638b634aa18939270493187
Wajud <wajudkareem757@gmail.com>
2023-10-11 20:53:41 +0100

    made header responsive

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-const submitPost = document.querySelectorAll(".post")
+const submitPost = document.querySelectorAll(".post");
 
-submitPost.forEach(submittedPost=>{
-      submittedPost.addEventListener("click", ()=>{
+submitPost.forEach((submittedPost) => {
+  submittedPost.addEventListener("click", () => {
+    const createdPost = document.querySelectorAll(".created-post");
 
-            const createdPost = document.querySelectorAll(".created-post")
+    let createdPostContent;
 
-            let createdPostContent;
-        
-            for(let i = 0; i < createdPost.length; i++){
-              
-                  if(createdPost[i].value != ""){
-                       createdPostContent = createdPost[i].value;
-                  }
+    for (let i = 0; i < createdPost.length; i++) {
+      if (createdPost[i].value != "") {
+        createdPostContent = createdPost[i].value;
+      } else {
+        continue;
+      }
+    }
 
-                  else{
-                        continue
-                  }
-            }
-
-            const topPost = document.querySelector("#top-post")
-            let postContainer = document.createElement("div")
-            postContainer.innerHTML = `<div class="mb-2">
+    const topPost = document.querySelector("#top-post");
+    let postContainer = document.createElement("div");
+    postContainer.innerHTML = `<div class="mb-2">
             <div
               class="bg-white md:mt-0 mt-4 flex items-center gap-2 py-2 px-4 relative rounded-md shadow-md"
             >
@@ -34,11 +30,38 @@ submitPost.forEach(submittedPost=>{
             >
             ${createdPostContent}
             </p>
-          </div>`
-            topPost.prepend(postContainer)
-            createdPost.forEach(created=>created.value="")
+          </div>`;
+    topPost.prepend(postContainer);
+    createdPost.forEach((created) => (created.value = ""));
+  });
+});
 
-      })
-})
+// Navbar Functionality
 
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const mobileMenu = document.getElementById("mobileMenu");
+const header = document.querySelector("header");
+let start = header.getBoundingClientRect().height;
 
+openMenu.addEventListener("click", () => {
+  mobileMenu.classList.replace("hidden", "block");
+  mobileMenu.classList.add("md:hidden");
+  if (mobileMenu.classList.contains("slide-out")) {
+    mobileMenu.classList.replace("slide-out", "slide-in");
+  } else {
+    mobileMenu.classList.add("slide-in");
+  }
+
+  openMenu.classList.replace("block", "hidden");
+  closeMenu.classList.replace("hidden", "block");
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.replace("slide-in", "slide-out");
+  openMenu.classList.replace("hidden", "block");
+  closeMenu.classList.replace("block", "hidden");
+  setTimeout(() => {
+    mobileMenu.classList.replace("block", "hidden");
+  }, 2000);
+});

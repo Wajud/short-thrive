176c74acec45eec60fcad6015df352d2cb313956
Wajud <wajudkareem757@gmail.com>
2023-10-12 01:21:20 +0100

    created responsive header and footer

--- main.js
+++ main.js
@@ -35,33 +35,3 @@ submitPost.forEach((submittedPost) => {
     createdPost.forEach((created) => (created.value = ""));
   });
 });
-
-// Navbar Functionality
-
-const openMenu = document.getElementById("openMenu");
-const closeMenu = document.getElementById("closeMenu");
-const mobileMenu = document.getElementById("mobileMenu");
-const header = document.querySelector("header");
-let start = header.getBoundingClientRect().height;
-
-openMenu.addEventListener("click", () => {
-  mobileMenu.classList.replace("hidden", "block");
-  mobileMenu.classList.add("md:hidden");
-  if (mobileMenu.classList.contains("slide-out")) {
-    mobileMenu.classList.replace("slide-out", "slide-in");
-  } else {
-    mobileMenu.classList.add("slide-in");
-  }
-
-  openMenu.classList.replace("block", "hidden");
-  closeMenu.classList.replace("hidden", "block");
-});
-
-closeMenu.addEventListener("click", () => {
-  mobileMenu.classList.replace("slide-in", "slide-out");
-  openMenu.classList.replace("hidden", "block");
-  closeMenu.classList.replace("block", "hidden");
-  setTimeout(() => {
-    mobileMenu.classList.replace("block", "hidden");
-  }, 2000);
-});
